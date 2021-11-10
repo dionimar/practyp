1d8d652db41aca1a287cc16fd93172128a854e61
dioni <dioni@main-workstation>
2021-11-10 18:34:11 +0100

    working but timers make non sense

--- practyp/src/main/scala/example/Hello.scala
+++ practyp/src/main/scala/example/Hello.scala
@@ -1,17 +1,12 @@
 package example
 
 
+//import cats.effect.unsafe.implicits.global
 //import cats.data.ContT
 import cats.effect._
 import scala.concurrent.duration._
 import scala.io.StdIn._
-
 import scala.math.pow
-
-// import scala.swing._
-// import scala.swing.event._
-
-import scala.tools.jline._
 
 
 
@@ -24,69 +19,49 @@ object Main extends IOApp.Simple {
     ((ending - start)).toSeconds
 
   def wordsPerMinute(inputString: String, duration: Double) =
-    inputString.split(" ").size * 60 / duration
+    inputString.split(" ").size * 60.0 / duration
 
   def compare(input: String, output: String) =
     input
       .zip(output)
       .map(x => x._1 == x._2)
 
-  // val consoleRead = new console.ConsoleReader()
-
   def putStr(str: String) = IO(println(str))
   def put_(str: String) = IO(print(str))
   val charReader = System.console().reader()
-  val rdchar = IO(charReader.read.toString)
-  //val rdchar = IO(readChar())
-  //val rdchar = IO(consoleRead.readCharacter())
-  // val rdline = IO(readLine())
+  val rdchar: IO[String] = IO(charReader.read.toChar.toString.mkString(""))
 
-  val rdline: IO[String] = IO {
-      Iterator
-        .continually(
-          {
-            val c = charReader.read
-            //print(c.toChar)
-            c.toString
-          }
-        )
-        .takeWhile( s => s.toInt >= 32 && s.toInt <= 126 && s.nonEmpty)
-        .toList
-        .map(_.toInt.toChar.toString)
-        .mkString("")
+
+  def rdline(inputString: String): String = {
+    Iterator
+      .continually(
+        {
+          val c = charReader.read;
+          c.toString;
+        }
+      )
+      .takeWhile( s => s.toInt >= 32 && s.toInt <= 126 && s.nonEmpty)
+      .toList
+      .map(_.toInt.toChar.toString)
+      .mkString("")
   }
-  
 
   def clearScreen = putStr("\033[2J")
 
-  // def performIteration(testString: String) = for {
-  //   _ <- clearScreen
-  //   _ <- putStr(testString)
-  //   start <- Clock[IO].monotonic
-  //   x <- IO(rdline)
-  //   ending <- Clock[IO].monotonic
-  //   results <- IO(wordsPerMinute(x, elapsedSeconds(start, ending)).toString + "WPM") 
-  // } yield results
-
-  val mainLoop: IO[Unit] = for {
-    // results <- performIteration("Type this if you can as fast as you can")
-    // _ <- putStr(results)
-
-
+  val mainLoop = for {
     _ <- clearScreen
     _ <- putStr("hola typea esto plz")
     start <- Clock[IO].monotonic
-    x <- rdline
-    //x <- IO.iterateWhile()
+    typedText <- IO(rdline("hola typea esto plz"))
     ending <- Clock[IO].monotonic
-    //_ <- putStr(x)
-    _ <- putStr(wordsPerMinute(x, elapsedSeconds(start, ending)).toString + "WPM")
+  } yield (typedText, start, ending)
 
-    //wait <- rdline
+  //val run = mainLoop
 
-    // results <- performIteration("En un lugar de la mancha de cuyo nombre no macuerdo")
-    // _ <- putStr(results)
+  val run = for {
+    results <- mainLoop
+    _ <- IO(println(wordsPerMinute(results._1, elapsedSeconds(results._2, results._3)).toString + "WPM"))
   } yield ()
 
-  val run = mainLoop
+  
 }
