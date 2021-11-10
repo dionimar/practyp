767080caa268224e6c6f7dac14afc1792d171e68
Dioni Martinez <33429845+dionimar@users.noreply.github.com>
2021-11-10 22:05:32 +0100

    refactor to lanterna. Semi working

--- practyp/src/main/scala/example/Hello.scala
+++ practyp/src/main/scala/example/Hello.scala
@@ -2,66 +2,138 @@ package example
 
 
 //import cats.effect.unsafe.implicits.global
-//import cats.data.ContT
+import cats.data.ContT
 import cats.effect._
 import scala.concurrent.duration._
 import scala.io.StdIn._
 import scala.math.pow
 
+import com.googlecode.lanterna.terminal.Terminal
+import com.googlecode.lanterna._
+import com.googlecode.lanterna.input.KeyType
+
+//import scala.utils.jline
+import java.util.Scanner
+import javax.swing.KeyStroke
 
 
-object Main extends IOApp.Simple {
+object Main extends App {
+  val defaultTerminalFactory = new terminal.DefaultTerminalFactory()
+  val term = defaultTerminalFactory.createTerminal()
+  term.clearScreen()
 
-  // lazy val mainLoop: IO[Unit] = IO.println("hola") >> mainLoop
-  // mainLoop.timeout(5.seconds)
+  val textGraphics = term.newTextGraphics()
 
-  def elapsedSeconds(start: FiniteDuration, ending: FiniteDuration) =
-    ((ending - start)).toSeconds
+  //var keyStroke = term.readInput()
 
-  def wordsPerMinute(inputString: String, duration: Double) =
-    inputString.split(" ").size * 60.0 / duration
-
-  def compare(input: String, output: String) =
-    input
-      .zip(output)
-      .map(x => x._1 == x._2)
-
-  def putStr(str: String) = IO(println(str))
-  def put_(str: String) = IO(print(str))
-  val charReader = System.console().reader()
-  val rdchar: IO[String] = IO(charReader.read.toChar.toString.mkString(""))
-
-
-  def rdline(inputString: String): String = {
-    Iterator
-      .continually(
-        {
-          val c = charReader.read;
-          c.toString;
-        }
-      )
-      .takeWhile( s => s.toInt >= 32 && s.toInt <= 126 && s.nonEmpty)
-      .toList
-      .map(_.toInt.toChar.toString)
-      .mkString("")
+  def getCharAndContinue(
+    inputString: String,
+    term: terminal.Terminal,
+    keyStroke: input.KeyStroke
+  ): Unit = {
+    keyStroke.getKeyType() match {
+      case KeyType.Escape => {}
+      case other => {
+        textGraphics.drawLine(5, 4, term.getTerminalSize().getColumns() - 1, 4, ' ')
+        textGraphics.putString(5, 4, "Last Keystroke: ", SGR.BOLD)
+        textGraphics.putString(5 + "Last Keystroke: ".length(), 4, keyStroke.toString())
+        term.flush()
+        val newKeyStroke = term.readInput()
+        getCharAndContinue(inputString, term, newKeyStroke)
+      }
+    }
   }
 
-  def clearScreen = putStr("\033[2J")
+  val ks = term.readInput()
+  getCharAndContinue("asdf df asdf", term, ks)
 
-  val mainLoop = for {
-    _ <- clearScreen
-    _ <- putStr("hola typea esto plz")
-    start <- Clock[IO].monotonic
-    typedText <- IO(rdline("hola typea esto plz"))
-    ending <- Clock[IO].monotonic
-  } yield (typedText, start, ending)
+  // Iterator.continually(
+  //   {
+  //     val keyStroke = term.readInput()
+  //     textGraphics.drawLine(5, 4, term.getTerminalSize().getColumns() - 1, 4, ' ')
+  //     textGraphics.putString(5, 4, "Last Keystroke: ", SGR.BOLD)
+  //     textGraphics.putString(5 + "Last Keystroke: ".length(), 4, keyStroke.toString())
+  //     term.flush()
+  //     keyStroke
+  //   }
+  // )
+  //   .takeWhile(_.getKeyType() != KeyType.Escape)
+  //   .toList
 
-  //val run = mainLoop
+  // while(keyStroke.getKeyType() != KeyType.Escape) {
+  //   textGraphics.drawLine(5, 4, term.getTerminalSize().getColumns() - 1, 4, ' ');
+  //   textGraphics.putString(5, 4, "Last Keystroke: ", SGR.BOLD);
+  //   textGraphics.putString(5 + "Last Keystroke: ".length(), 4, keyStroke.toString());
+  //   term.flush();
+  //   keyStroke = term.readInput();
+  // }
 
-  val run = for {
-    results <- mainLoop
-    _ <- IO(println(wordsPerMinute(results._1, elapsedSeconds(results._2, results._3)).toString + "WPM"))
-  } yield ()
+  // val in = Iterator.continually(
+
+  // )
+
+
+}
+
+
+// object Main extends IOApp.Simple {
+
+//   // lazy val mainLoop: IO[Unit] = IO.println("hola") >> mainLoop
+//   // mainLoop.timeout(5.seconds)
+
+//   def elapsedSeconds(start: FiniteDuration, ending: FiniteDuration) =
+//     ((ending - start)).toSeconds
+
+//   def wordsPerMinute(inputString: String, duration: Double) =
+//     inputString.split(" ").size * 60.0 / duration
+
+//   def compare(input: String, output: String) =
+//     input
+//       .zip(output)
+//       .map(x => x._1 == x._2)
+
+//   def putStr(str: String) = IO(println(str))
+//   def put_(str: String) = IO(print(str))
+//   val charReader = System.console().reader()
+//   //val charReader = new Scanner(System.in)
+//   //val rdchar: IO[String] = IO(charReader.read.toChar.toString.mkString(""))
+
+
+//   def rdline(inputString: String): String = {
+//     Iterator
+//       .continually(
+//         {
+//           val c = charReader.read;
+//           //val c = charReader.next()
+//           //print(c)
+//           c.toString;
+//         }
+//       )
+//       .takeWhile( s => s.toInt >= 32 && s.toInt <= 126 && s.nonEmpty)
+//       .toList
+//       .map(_.toInt.toChar.toString)
+//       .mkString("")
+//   }
+
+//   def clearScreen = putStr("\033[2J")
+
+//   def mainLoop() = for {
+//     _ <- clearScreen
+//     _ <- putStr("hola typea esto plz")
+//     start <- Clock[IO].monotonic
+//     typedText <- IO(rdline("hola typea esto plz"))
+//     ending <- Clock[IO].monotonic
+//     _ <- putStr(
+//       wordsPerMinute(typedText, elapsedSeconds(start, ending)).toString + "WPM"
+//     )
+//   } yield ()
+
+//   val run = mainLoop()
+
+//   // val run = for {
+//   //   results <- mainLoop
+//   //   _ <- 
+//   // } yield ()
 
   
-}
+// }
