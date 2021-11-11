be7ff7c903c46beb6ab282b485f192a96b9a377e
dioni <dioni@main-workstation>
2021-11-11 10:57:43 +0100

    env moved to class

--- practyp/src/main/scala/example/Hello.scala
+++ practyp/src/main/scala/example/Hello.scala
@@ -26,16 +26,28 @@ object Main extends IOApp.Simple {
 
   //var keyStroke = term.readInput()
 
-  val env = (term, textGraphics)
+  class termEnv(val term: terminal.Terminal, val graphs: graphics.TextGraphics)
+
+  val env = new termEnv(term, textGraphics)
+
+  def drawCurrentState(env: termEnv, inputString: String, typedString: String): IO[Unit] = {
+    val term = env.term
+    val graphs = env.graphs
+    for {
+      _ <- IO(graphs.putString(5, 2, inputString, SGR.BOLD))
+      _ <- IO(graphs.putString(5, 4, typedString))
+      _ <- IO(term.flush())
+    } yield ()
+  }
 
   def getCharAndContinue(
     inputString: String,
     typedString: String,
-    env: (terminal.Terminal, graphics.TextGraphics),
-    keyStroke: input.KeyStroke): IO[(String, (terminal.Terminal, graphics.TextGraphics))] =
+    env: termEnv,
+    keyStroke: input.KeyStroke): IO[(String, termEnv)] =
   {
-    val term = env._1
-    val graphs = env._2
+    val term = env.term
+    val graphs = env.graphs
     keyStroke.getKeyType() match {
       case KeyType.Escape => IO {term.clearScreen(); (typedString, env)}
       case KeyType.Enter  => IO {term.clearScreen(); (typedString, env)}
@@ -53,10 +65,8 @@ object Main extends IOApp.Simple {
       }
       case other => {
         for {
-          _ <- IO(graphs.putString(5, 2, inputString, SGR.BOLD))
           newTypedString <- IO(typedString + keyStroke.getCharacter().toString())
-          _ <- IO(graphs.putString(5, 4, newTypedString))
-          _ <- IO(term.flush())
+          _ <- drawCurrentState(env, inputString, newTypedString)
           newKeyStroke <- IO(term.readInput())
           typedResult <- getCharAndContinue(inputString, newTypedString, env, newKeyStroke)
         } yield ((typedResult._1, env))
@@ -87,64 +97,3 @@ object Main extends IOApp.Simple {
 }
 
 
-// object Main extends IOApp.Simple {
-
-//   // lazy val mainLoop: IO[Unit] = IO.println("hola") >> mainLoop
-//   // mainLoop.timeout(5.seconds)
-
-//   def elapsedSeconds(start: FiniteDuration, ending: FiniteDuration) =
-//     ((ending - start)).toSeconds
-
-//   def wordsPerMinute(inputString: String, duration: Double) =
-//     inputString.split(" ").size * 60.0 / duration
-
-//   def compare(input: String, output: String) =
-//     input
-//       .zip(output)
-//       .map(x => x._1 == x._2)
-
-//   def putStr(str: String) = IO(println(str))
-//   def put_(str: String) = IO(print(str))
-//   val charReader = System.console().reader()
-//   //val charReader = new Scanner(System.in)
-//   //val rdchar: IO[String] = IO(charReader.read.toChar.toString.mkString(""))
-
-
-//   def rdline(inputString: String): String = {
-//     Iterator
-//       .continually(
-//         {
-//           val c = charReader.read;
-//           //val c = charReader.next()
-//           //print(c)
-//           c.toString;
-//         }
-//       )
-//       .takeWhile( s => s.toInt >= 32 && s.toInt <= 126 && s.nonEmpty)
-//       .toList
-//       .map(_.toInt.toChar.toString)
-//       .mkString("")
-//   }
-
-//   def clearScreen = putStr("\033[2J")
-
-//   def mainLoop() = for {
-//     _ <- clearScreen
-//     _ <- putStr("hola typea esto plz")
-//     start <- Clock[IO].monotonic
-//     typedText <- IO(rdline("hola typea esto plz"))
-//     ending <- Clock[IO].monotonic
-//     _ <- putStr(
-//       wordsPerMinute(typedText, elapsedSeconds(start, ending)).toString + "WPM"
-//     )
-//   } yield ()
-
-//   val run = mainLoop()
-
-//   // val run = for {
-//   //   results <- mainLoop
-//   //   _ <- 
-//   // } yield ()
-
-  
-// }
