c91daa6438f8a03744de424325dbf21c1febfcd5
dioni <dioni@main-workstation>
2021-11-12 09:16:49 +0100

    added interface for environment and create a raw term based one

--- practyp/src/main/scala/example/Hello.scala
+++ practyp/src/main/scala/example/Hello.scala
@@ -3,8 +3,10 @@ package example
 import cats.data.ContT
 import cats.effect._
 import scala.concurrent.duration._
-import scala.io.StdIn._
+import scala.io.StdIn.{readLine => scalaReadLine}
 import scala.math.pow
+
+import scala.swing._
 
 import com.googlecode.lanterna.terminal.Terminal
 import com.googlecode.lanterna._
@@ -24,107 +26,150 @@ object stringJoiner {
           case _ => "\u001B[38;5;161m" + x._1
         }
       })
-      .mkString("")
+      .mkString("") + "\u001B[0m"
 }
 
 
-class termEnv(val term: terminal.Terminal, val graphs: graphics.TextGraphics)
+
+trait faceEnvironment {
+  def flush()
+  def read()
+  def clear()
+  def readLine(): String
+  def print(col: Int, row: Int, str: String)
+  def delChar()
+}
+
+case object rawTerm extends faceEnvironment {
+  def flush() = {}
+  def read() = {}
+  def clear() = {println("\u001b[2J")}
+  def readLine() = scalaReadLine()
+  def print(col: Int, row: Int, str: String) =
+    println(
+        Iterator.continually({"\t"}).take(col).mkString("") + str
+    )
+  def delChar() = {}
+}
+
+
+
+// class termEnv(val term: terminal.Terminal, val graphs: graphics.TextGraphics) {
+//   def flush() = term.flush()
+//   def read() = term.readInput()
+//   def clear() = term.clearScreen()
+//   def readLine() = scalaReadLine()
+//   def print(col: Int, row: Int, str: String) = graphs.putString(col, row, str)
+//   def delChar() = {
+//     term.setCursorPosition(term.getCursorPosition().withRelativeColumn(-1))
+//     term.putCharacter(' ')
+//   }
+// }
+
+// object termEnv {
+//   def apply() = {
+//     val defaultTerminalFactory = new terminal.DefaultTerminalFactory()
+//     val term = defaultTerminalFactory.createTerminal()
+//     val textGraphics = term.newTextGraphics()
+//     new termEnv(term, textGraphics)
+//   }
+// }
 
 
 object Main extends IOApp.Simple {
-  val defaultTerminalFactory = new terminal.DefaultTerminalFactory()
-  val term = defaultTerminalFactory.createTerminal()
-  term.clearScreen()
 
-  val textGraphics = term.newTextGraphics()
-
-  val env = new termEnv(term, textGraphics)
-
-
-  def deleteLastChar(env: termEnv): IO[Unit] = {
+  def deleteLastChar(env: faceEnvironment): IO[Unit] = {
     for {
-      _ <- IO(env.term.setCursorPosition(term.getCursorPosition().withRelativeColumn(-1)))
-      _ <- IO(env.term.putCharacter(' '))
-      _ <- IO(env.term.flush())
+      _ <- IO(env.delChar())
+      _ <- IO(env.flush())
     } yield ()
   }
 
-  def drawCurrentState(env: termEnv, inputString: String, typedString: String): IO[Unit] = {
+  def drawCurrentState(env: faceEnvironment, inputString: String, typedString: String): IO[Unit] = {
     val formatted = stringJoiner.join(inputString, typedString)
     for {
-      _ <- IO(env.graphs.putString(5, 2, inputString, SGR.BOLD))
-      _ <- IO(env.graphs.putString(5, 4, typedString))
-      _ <- IO(env.term.flush())
+      _ <- IO(env.print(5, 2, inputString))
+      _ <- IO(env.print(5, 4, typedString))
+      _ <- IO(env.flush())
     } yield ()
   }
 
-  def drawStatistics(env: termEnv, inputString: String, targetString: String, time: Double): IO[Unit] = {
+  def drawStatistics(env: faceEnvironment, inputString: String, targetString: String, time: Double): IO[Unit] = {
     val wpm = inputString.split(" ").size.toDouble * 60 / time
     //val acc = 
     for {
-      _ <- IO(env.graphs.putString(5, 2, targetString, SGR.BOLD))
-      _ <- IO(env.graphs.putString(5, 4, "Speed " + wpm + " WPM"))
-      _ <- IO(env.graphs.putString(5, 5, "Accuracy"))
+      _ <- IO(env.print(5, 2, stringJoiner.join(inputString, targetString)))
+      _ <- IO(env.print(5, 4, "Speed " + wpm + " WPM"))
+      _ <- IO(env.print(5, 5, "Accuracy"))
     } yield()
   }
 
-  def getCharAndContinue(
-    inputString: String,
-    typedString: String,
-    env: termEnv,
-    keyStroke: input.KeyStroke): IO[(String, termEnv)] =
-  {
-    keyStroke.getKeyType() match {
-      case KeyType.Escape => IO {env.term.clearScreen(); (typedString, env)}
-      case KeyType.Enter  => IO {env.term.clearScreen(); (typedString, env)}
-      case KeyType.Delete | KeyType.Backspace => {
-        val newTypedString = typedString.dropRight(1)
-        for {
-          _ <- deleteLastChar(env)
-          _ <- drawCurrentState(env, inputString, newTypedString)
-          newKeyStroke <- IO(env.term.readInput())
-          typedResult <- getCharAndContinue(inputString, newTypedString, env, newKeyStroke)
-        } yield ((typedResult._1, env))
-      }
-      case other => {
-        val newTypedString = typedString + keyStroke.getCharacter().toString()
-        for {
-          _ <- drawCurrentState(env, inputString, newTypedString)
-          newKeyStroke <- IO(env.term.readInput())
-          typedResult <- getCharAndContinue(inputString, newTypedString, env, newKeyStroke)
-        } yield ((typedResult._1, env))
-      }
-    }
+  // def getCharAndContinue(
+  //   inputString: String,
+  //   typedString: String,
+  //   env: termEnv,
+  //   keyStroke: input.KeyStroke): IO[(String, termEnv)] =
+  // {
+  //   keyStroke.getKeyType() match {
+  //     case KeyType.Escape => IO {env.clear(); (typedString, env)}
+  //     case KeyType.Enter  => IO {env.clear(); (typedString, env)}
+  //     case KeyType.Delete | KeyType.Backspace => {
+  //       val newTypedString = typedString.dropRight(1)
+  //       for {
+  //         _ <- deleteLastChar(env)
+  //         _ <- drawCurrentState(env, inputString, newTypedString)
+  //         newKeyStroke <- IO(env.read())
+  //         typedResult <- getCharAndContinue(inputString, newTypedString, env, newKeyStroke)
+  //       } yield ((typedResult._1, env))
+  //     }
+  //     case other => {
+  //       val newTypedString = typedString + keyStroke.getCharacter().toString()
+  //       for {
+  //         _ <- drawCurrentState(env, inputString, newTypedString)
+  //         newKeyStroke <- IO(env.read())
+  //         typedResult <- getCharAndContinue(inputString, newTypedString, env, newKeyStroke)
+  //       } yield ((typedResult._1, env))
+  //     }
+  //   }
+  // }
+
+  def readLine(env: faceEnvironment): IO[String] = {
+    for {
+      typedString <- IO(env.readLine())
+    } yield (typedString)
   }
 
-  def startTyping(env: termEnv, inputString: String): IO[(String, Double)] = {
+  def startTyping(env: faceEnvironment, inputString: String): IO[(String, Double)] = {
     for {
       _ <- drawCurrentState(env, inputString, "")
-      kp <- IO(env.term.readInput())
+      //kp <- IO(env.read())
       startTimer <- Clock[IO].monotonic
-      typedResult <- getCharAndContinue(inputString, "", env, kp)
+      //typedResult <- getCharAndContinue(inputString, "", env, kp) // If enable, take firs comp
+      typedResult <- readLine(env)
       endTimer <- Clock[IO].monotonic
-    } yield ((typedResult._1, (endTimer - startTimer).toSeconds))
+    } yield ((typedResult, (endTimer - startTimer).toSeconds))
   }
 
-  def mainLoop(env: termEnv): IO[Unit] = {
+  def mainLoop(env: faceEnvironment): IO[Unit] = {
     for {
-      _ <- IO(env.term.clearScreen())
+      _ <- IO(env.clear())
       results <- startTyping(env, "Type this as fast as you can")
       elapsedTime <- IO(results._2)
       typedString <- IO(results._1)
-      //_ <- drawCurrentState(env, typedString, "Elapsed time: " + elapsedTime + " s.")
       _ <- drawStatistics(env, "Type this as fast as you can", typedString, elapsedTime)
     } yield ()
   }
 
-  def run() = for {
-    _ <- mainLoop(env)
-    _ <- IO(env.graphs.putString(5, 8, "Type r to repeat, q to quit."))
-    choice <- IO(env.term.readInput())
-    _ <- choice.getCharacter().toString() match {
-        case "q" => IO{}
-        case "r" => run()
-      }
-  } yield ()
+  def run() = {
+    val env = rawTerm
+    for {
+      _ <- mainLoop(env)
+      _ <- IO(env.print(5, 8, "Type r to repeat, q to quit."))
+      // choice <- IO(env.read())
+      // _ <- choice.getCharacter().toString() match {
+      //     case "q" => IO{}
+      //     case "r" => run()
+      //   }
+    } yield ()
+  }
 }
